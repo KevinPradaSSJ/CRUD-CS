62f14775ed9d5d18e843072238c6bc9a9a3452a0
KevinPradaSSJ <santicastellanos2017@gmail.com>
2025-08-08 10:53:50 -0500

    Proceso de función eliminar

--- Controllers/Tienda_CS/ProductoControlador.cs
+++ Controllers/Tienda_CS/ProductoControlador.cs
@@ -1,8 +1,22 @@
-﻿
+﻿using Models.Tienda_CS;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Tienda_CS;
+
+
 namespace Controllers.Tienda_CS
 {
-    internal class ProductoControlador
+    public class ProductoControlador
     {
         //CLASE PARA EL CONTROLADOR DE PRODUCTO
+
+        public bool EliminarProducto(int id)
+        {
+            // Lógica para eliminar un producto por su ID
+            string query = $"DELETE FROM productos WHERE id_producto = {id}";
+            int result = DatabaseConnection.ExecuteNonQuery(query);
+            return result > 0; // Retorna true si se eliminó al menos un producto
+        }
     }
 }
--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+656e6b00149c71f90cad03c394025f70af80af73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87a1828f50a9bd2ebef12c462b33a47a028994e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
