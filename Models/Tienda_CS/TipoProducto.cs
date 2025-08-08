6add26cb3c740de11535d4a2b508b731f6d2011d
Camilo Hernandez <ch231322@gmail.com>
2025-08-08 10:19:13 -0500

    Acomodando nombres de las realciones de la DB con la conexion

--- Models/Tienda_CS/TipoProducto.cs
+++ Models/Tienda_CS/TipoProducto.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace Models.Tienda_CS
 {
@@ -10,12 +11,14 @@ namespace Models.Tienda_CS
 
         [Key]
         [Display(Name = "#")]
-        public int Id { get; set; }     // ID único del tipo de producto
-
+        public int IdTipo { get; set; }     // ID único del tipo de producto (coincide con id_tipo en BD)
 
         [Required(ErrorMessage = "El nombre del tipo de producto es obligatorio")]
         [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres")]
         [Display(Name = "Nombre del tipo de producto")]
         public string Nombre { get; set; }   // Nombre del tipo de producto
+
+        // Propiedad de navegación para la relación con Producto (uno a muchos)
+        public virtual ICollection<Producto> Productos { get; set; }
     }
 }
